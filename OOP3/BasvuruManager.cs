6d7f80c3a6ebc54c4c894fbabbe1aacfca4095a8
emtaha2 <77552332+emtaha2@users.noreply.github.com>
2021-01-24 02:31:48 +0300

    Add files via upload

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Başvuru bilgileri değerlendirme
+            //
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+            public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+            {
+                foreach (var kredi in krediler)
+                {
+                    kredi.Hesapla();
+                }
+            }
+        
+    }
+}
