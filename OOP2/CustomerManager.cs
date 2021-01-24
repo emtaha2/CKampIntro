410f14f9cfb06a96190028e1d9f87aa2f116ae17
emtaha2 <77552332+emtaha2@users.noreply.github.com>
2021-01-24 17:09:52 +0300

    Add files via upload

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+
+    //İş sınıfları // business classes
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            //if (customer.)
+            //{
+
+            //}
+            Console.WriteLine(customer.Id + " Üyelik aktifleştirildi.");
+            Console.WriteLine(customer.CustomerNo + " Üyelik aktifleştirildi.");
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Customer customer1 = new Customer();
+            //    customer1.Name = "Sena";
+            //    customer1.Surname = "Uğur";
+            //    customer1.Id = 1;
+            //    customer1.IdNo = "125415625470";
+            //    customer1.CustomerNo = "110025";
+            //    customer1.CompanyName = "!";
+
+
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNo = "0001";
+            customer1.Name = "Sena";
+            customer1.Surname = "Uğur";
+            customer1.TcNo = "12345678912";
+
+
+            CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "00021";
+            customer2.CompanyName = "CTC";
+            customer2.TaxNo = "12385214";
+
+
+
+            //SOLID
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CoorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer1);
+
+        }
+    }
+}
