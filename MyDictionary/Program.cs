2072291f12944626e6a38af1964f965ff8095cbb
emtaha2 <77552332+emtaha2@users.noreply.github.com>
2021-01-31 19:11:51 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using OdevDictionary;
+using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Sena");
+            dictionary.Add(12, "emir");
+        }
+    }
+}
